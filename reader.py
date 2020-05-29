c76d7d36740aeb409e38d49bfb25bb13b863b530
Patryk Stronski <p.stronski13@gmail.com>
2020-05-29 14:01:52 +0200

    reader.py alligned

--- reader.py
+++ reader.py
@@ -15,14 +15,14 @@ client.username_pw_set(username='client', password='client')
 client.connect(consts.QUEUE_ADDRESS, consts.PORT)
 terminal_id = str(uuid.uuid4())
 
-def ​process_message​(​client​, ​userdata​, message):
-    message_decoded = json.loads​(message.payload.decode(​"utf-8"​))
-    messagebox.showinfo(​"Message from the Server"​, message_decoded["status"] + message_decoded["received"])
+def process_ack_message(client, userdata, message):
+    print(message.payload.decode("utf-8"))
 
-def save_uid_log(uid, time_read):
+def save_uid_log(uid,time_read):
     s = "-"
     time_read=int(time_read)
     msg = {"status": "log", "time": time_read, "uid": s.join(uid), "instance_id": terminal_id}
+    print("Sending message to server")
     client.publish(consts.QUEUE_TOPIC, json.dumps(msg))
 
 def initialize_reader():
@@ -35,14 +35,16 @@ def initialize_reader():
       while GPIO.input(buttonRed):
         (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
         (status,uid) = MIFAREReader.MFRC522_Anticoll()
+        client.loop_start()
+        client.subscribe(consts.QUEUE_TOPIC_ACK + "/" + terminal_id, 2)
         if status == MIFAREReader.MI_OK:
           save_uid_time(uid,time.time())
           print("UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
-          time.sleep(1)
+          time.sleep(1)   
     except KeyboardInterrupt:
         msg = {"status": "health", "on": 0, "instance_id": terminal_id}
         client.publish(consts.QUEUE_TOPIC, json.dumps(msg))
         GPIO.cleanup()
 
-initialize_reader()
 client.on_message = ​process_ack_message​
+initialize_reader()
